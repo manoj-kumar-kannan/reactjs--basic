f39ffb7193deb1cd9120385ce7aa7e85889190ba
MK Tutorials <manojkumar3694@gmail.com>
2023-04-16 20:16:41 +0530

    install bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
